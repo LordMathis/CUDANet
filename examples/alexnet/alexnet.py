88e852a6e5addfb1cc4427c36aeaf7673f9b6ae3
LordMathis <matus@namesny.com>
2024-04-22 22:04:46 +0200

    Fix missing predict argument

--- examples/alexnet/alexnet.py
+++ examples/alexnet/alexnet.py
@@ -34,5 +34,5 @@ if __name__ == "__main__":
     alexnet = torchvision.models.alexnet(weights=torchvision.models.AlexNet_Weights.DEFAULT)
     print_model_parameters(alexnet)  # print layer names and number of parameters
     export_model_weights(alexnet, 'alexnet_weights.bin')
-    # predict('cat.jpg')
+    # predict(alexnet, 'cat.jpg')
 
