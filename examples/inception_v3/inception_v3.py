da95fa203e2f3940a1ebf40f70957f5d58fb1802
LordMathis <matus@namesny.com>
2024-05-30 13:19:11 +0200

    Update inception py

--- examples/inception_v3/inception_v3.py
+++ examples/inception_v3/inception_v3.py
@@ -1,17 +1,14 @@
-import torch
 import torchvision
 import sys
-
-from torchsummary import summary
-
-inception = torchvision.models.inception_v3(weights=torchvision.models.Inception_V3_Weights.DEFAULT)
-inception.eval()
 
 sys.path.append('../../tools')  # Ugly hack 
 from utils import export_model_weights, print_model_parameters
 
-print_model_parameters(inception)  # print layer names and number of parameters
 
-inception.cuda()
+if __name__ == "__main__":
+    inception = torchvision.models.inception_v3(weights=torchvision.models.Inception_V3_Weights.DEFAULT)
+    inception.eval()
 
-summary(inception, (3, 299, 299))
+    print_model_parameters(inception)  # print layer names and number of parameters
+
+    export_model_weights(inception, 'inception_v3_weights.bin')
